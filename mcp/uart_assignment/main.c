ef8b93f672dc56920e4ce02c79f3f6c02966ad19
KhoaHV1 <hoangvankhoa@outlook.com>
2017-08-31 09:08:55 +0700

    Update project files

--- mcp/uart_assignment/main.c
+++ mcp/uart_assignment/main.c
@@ -5,20 +5,22 @@
 /*
  * [File Name]     main.c
  * [Platform]      MKL46Z256VLH4
- * [Project]       exception_model
+ * [Project]       uart_assignment
  * [Version]       1.00
  * [Author]        KhoaHV1
- * [Date]          21-08-2017
+ * [Date]          28-08-2017
  * [Language]      'C'
  * [History]       1.00 - Original Release
  *
  */
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#include "gpio.h"
+#include "MKL46Z4.h"
 #include "board.h"
+#include "gpio.h"
+#include "pit.h"
 #include "port.h"
 #include "uart.h"
 
@@ -26,54 +28,145 @@
  * Definitions
  ******************************************************************************/
 
+#define BAUD_RATE (115200U)
+#define BUFF_SIZE (256U)
+
 /*******************************************************************************
  * Global variables
  ******************************************************************************/
+/* Count the second of clock (from 0-59) */
+uint8_t g_secCnt = -1;
+/* Count the minute of clock (from 0-99) */
+uint8_t g_minCnt = 0;
 
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
+/*!
+ * @brief Called every second.
+ */
+void secondTickHandler(void);
 
 /*!
- * @brief Handle SW1, SW3 pressed.
+ * @brief Copy an integer number to a buffer as a string.
+ *
+ * @param val  [in]     Integer number.
+ * @param buff [in,out] Buffer store string of `val`. `buff` must be wide enough
+ * to store the string.
+ *
+ * @return Return number of numeric character in `val`.
  */
-void PORTC_PORTD_IRQHandler(void);
-/*!
- * @brief Handle SysTick Interrupt.
- */
-void SysTick_Handler(void);
+uint8_t int2str(uint32_t val, uint8_t buff[]);
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
+void secondTickHandler(void)
+{
+    /* Store bytes to send */
+    uint8_t buff[BUFF_SIZE] = {0};
+
+    /* Number of bytes will be sent */
+    uint8_t n = 0;
+
+    /* Increase the second each second */
+    ++g_secCnt;
+
+    if (g_secCnt > 59)
+    {
+        /* Increase the minute and reset the second */
+        ++g_minCnt;
+        g_secCnt = 0;
+    }
+
+    if (g_minCnt > 99)
+    {
+        /* Reset the minute if it reached max value */
+        g_minCnt = 0;
+    }
+
+    n = int2str(g_secCnt, buff);
+    buff[n++] = ':';
+    n += int2str(g_minCnt, buff + n);
+    buff[n++] = '\r';
+    buff[n++] = '\n';
+
+    /* Print format: "ss:mm" */
+    UART_sendBytes(UART_0, buff, n);
+}
+
+uint8_t int2str(uint32_t val, uint8_t buff[])
+{
+    /* Indexing variables */
+    uint8_t i = 0;
+    uint8_t j = 0;
+
+    /* Number of numeric characters in val */
+    uint8_t n = 0;
+
+    /* Copy `val` to `buff` by reversed-order */
+    for (i = 0; val != 0; ++i)
+    {
+        buff[i] = val % 10 + '0';
+        val = val / 10;
+        ++n;
+    }
+
+    /* Reverse buff */
+    for (j = 0; j < i / 2; ++j)
+    {
+        buff[j] ^= buff[i - j - 1];
+        buff[i - j - 1] ^= buff[j];
+        buff[j] ^= buff[i - j - 1];
+    }
+
+    if (n < 1)
+    {
+        /* If `val == 0` then copy '00' to `buff` */
+        buff[0] = '0';
+        buff[1] = '0';
+        n = 2;
+    }
+
+    if (n < 2)
+    {
+        /* If `val < 10` then copy "0x" to `buff` (x == `val`) */
+        buff[1] = buff[0];
+        buff[0] = '0';
+        n = 2;
+    }
+
+    return n;
+}
 
 int main(void)
 {
-    /* Gợi ý: tạo ra uart_config_struct_t để chứa các config */
-    /* ... */
-    /* Enable clock for UART0 */
-    SIM_SCGC4 |= SIM_SCGC4_UART0_MASK;
-    /* Select clock source */
-    SIM_SOPT2 &= ~SIM_SOPT2_UART0SRC_MASK;
-    /* Enable UART0_RX */
-    PORTA->PCR[1] &= ~PORT_PCR_MUX_MASK;
-    PORTA->PCR[1] |= PORT_PCR_MUX(1);
-    /* Enable UART0_TX*/
-    PORTA->PCR[2] &= ~PORT_PCR_MUX_MASK;
-    PORTA->PCR[2] |= PORT_PCR_MUX(1);
+    /* Store configuration of PIT channel 0 */
+    pit_chnl_conf_t pitConf;
 
-    /* Configure the UART control registers for the desired data format */
-    /*  Number of data bits */
-    /* UART0->C1 */
-    /* Parity and parity type */
+    /* Store configuration of UART0 */
+    uart_conf_t uartConf;
 
-    /* MSB or LSB first */
+    PIT_enable();
 
-    /* Data polarity */
+    /* Configure timer 0 */
+    pitConf.time = 1000;
+    pitConf.handler = secondTickHandler;
+    pitConf.chn = false;
 
-    /* Configure the baud rate */
+    PIT_configChannel(0, &pitConf);
+    PIT_startChannel(0);
 
-    /* Enable the receiver and/or transmitter */
+    /* Configure UART0 */
+    uartConf.type = UART_TYPE_TRANSMITTER_MASK;
+    uartConf.sz = 8;
+    uartConf.parityEnable = false;
+    uartConf.msbf = false;
+    uartConf.polarity = 0;
+    uartConf.baudRate = BAUD_RATE;
+
+    UART_enable(UART_0);
+    UART_config(UART_0, &uartConf);
 
     while (1)
     {
---
+++ mcp/uart_assignment/pit.c
@@ -0,0 +1,123 @@
+#include <stddef.h>
+
+#include "MKL46Z4.h"
+#include "pit.h"
+
+/*******************************************************************************
+ * Macros
+ ******************************************************************************/
+/*!
+ * @brief Calculate value for LDVAL register from period (ms).
+ *
+ * @param T Period (ms).
+ *
+ * @return Value for LDVAL register.
+ */
+#define LDVAL_GET_COUNT(T) ((T) / 1000 * SystemCoreClock / 2 - 1)
+
+/*******************************************************************************
+ * Global variables
+ ******************************************************************************/
+
+/* Number of PIT channels */
+extern uint8_t g_pitChnlNum;
+
+extern void (*g_pitIrqHandler[])(void);
+
+/* Default system clock value */
+extern uint32_t SystemCoreClock;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+
+void PIT_IRQHandler(void)
+{
+    /* Channel index */
+    uint8_t i = 0;
+
+    for (i = 0; i < g_pitChnlNum; ++i)
+    {
+        if (PIT->CHANNEL[i].TFLG != PIT_TFLG_OCCURRED)
+        {
+            /* Channel i: timeout has not yet occurred */
+            continue;
+        }
+
+        /* Channel i: timeout has occurred */
+        /* Reset PIT interrupt flag */
+        PIT->CHANNEL[i].TFLG = 1;
+
+        /* Call handler if there is one */
+        if (g_pitIrqHandler[i] != NULL)
+        {
+            g_pitIrqHandler[i]();
+        }
+    }
+}
+
+void PIT_enable()
+{
+    /* Enable clock */
+    SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
+
+    /* Enable interrupt */
+    NVIC_ClearPendingIRQ(PIT_IRQn);
+    NVIC_EnableIRQ(PIT_IRQn);
+    NVIC_SetPriority(PIT_IRQn, 0);
+
+    /* Turn on PIT */
+    PIT->MCR = 0x00;
+}
+
+void PIT_startAll()
+{
+    /* Channel's index */
+    uint8_t i = 0;
+    for (i = 0; i < g_pitChnlNum; ++i)
+    {
+        PIT->CHANNEL[i].TCTRL |= PIT_TCTRL_TEN_MASK;
+    }
+}
+
+void PIT_startChannel(uint8_t idx)
+{
+    PIT->CHANNEL[idx].TCTRL |= PIT_TCTRL_TEN_MASK;
+}
+
+void PIT_stopAll()
+{
+    /* Channel's index */
+    uint8_t i = 0;
+    for (i = 0; i < g_pitChnlNum; ++i)
+    {
+        PIT->CHANNEL[i].TCTRL &= ~PIT_TCTRL_TEN_MASK;
+    }
+}
+
+void PIT_stopChannel(uint8_t idx)
+{
+    PIT->CHANNEL[idx].TCTRL &= ~PIT_TCTRL_TEN_MASK;
+}
+
+void PIT_configChannel(uint8_t idx, pit_chnl_conf_t *conf)
+{
+    g_pitIrqHandler[idx] = conf->handler;
+
+    PIT->CHANNEL[idx].LDVAL = LDVAL_GET_COUNT(conf->time);
+
+    /* Set value for TCTRL register */
+    PIT->CHANNEL[idx].TCTRL = 0;
+
+    if (conf->handler)
+    {
+        /* Enable interrupt handler */
+        PIT->CHANNEL[idx].TCTRL |= PIT_TCTRL_TIE_MASK;
+    }
+
+    if (conf->chn)
+    {
+        /* Enable chain mode */
+        PIT->CHANNEL[idx].TCTRL |= PIT_TCTRL_CHN_MASK;
+    }
+}
