10502659439443b30eac6273aff471fed7fe01f1
KhoaIO <hoangvankhoa@outlook.com>
2017-09-13 07:31:51 +0700

    To KhoaHV1

--- mcp/bootloader_mocktest/uart.c
+++ mcp/bootloader_mocktest/uart.c
@@ -19,10 +19,6 @@
  * Macros
  ******************************************************************************/
 
-#define UART_BAUD_RATE_GEN(clock, sbr, osr) ((clock) / ((sbr) * ((osr) + 1)))
-
-#define UART_GET_SBR(clock, baud, osr) ((clock) / ((baud) * ((osr) + 1)))
-
 #define ABS(v) ((v) >= 0 ? v : -v)
 
 /*******************************************************************************
@@ -57,42 +53,25 @@ void UART_enable(uint8_t uartx)
 
     /* Select clock source */
     SIM_SOPT2 |= SIM_SOPT2_UART0SRC(1);
+
+    /* Disable UART0 before setting up configuration */
+    /* Disable UART0_TX*/
+    PORTA->PCR[2] &= ~PORT_PCR_MUX_MASK;
+    /* Disable UART0_RX */
+    PORTA->PCR[1] &= ~PORT_PCR_MUX_MASK;
+    /* Disable interrupt */
+    NVIC_DisableIRQ(UART0_IRQn);
+    NVIC_ClearPendingIRQ(UART0_IRQn);
 }
 
 void UART_config(uint8_t uartx, uart_conf_t *conf)
 {
-    uint8_t osr = 0;
     uint16_t sbr = 0;
-    uint32_t baud = 0;
-
-    uint32_t delta = 0;
-
-    uint32_t minDelta = -1;
-    uint32_t minOsr = 0;
-    uint16_t minSbr = 0;
 
     if (uartx != UART_0)
     {
+        /* Only support UART0 for now */
         return;
-    }
-
-    if (conf->type & UART_TYPE_TRANSMITTER_MASK)
-    {
-        /* Enable UART0_TX*/
-        PORTA->PCR[2] &= ~PORT_PCR_MUX_MASK;
-        PORTA->PCR[2] |= PORT_PCR_MUX(2);
-    }
-
-    if (conf->type & UART_TYPE_RECEIVER_MASK)
-    {
-        /* Enable UART0_RX */
-        PORTA->PCR[1] &= ~PORT_PCR_MUX_MASK;
-        PORTA->PCR[1] |= PORT_PCR_MUX(2);
-
-        /* Enable receiver interrupt */
-        NVIC_ClearPendingIRQ(UART0_IRQn);
-        NVIC_EnableIRQ(UART0_IRQn);
-        NVIC_SetPriority(UART0_IRQn, 0);
     }
 
     /*  Number of data bits */
@@ -129,25 +108,12 @@ void UART_config(uint8_t uartx, uart_conf_t *conf)
         : UART0_C3_TXINV(0);
 
     /* Configure the baud rate */
-    for (osr = 4; osr <= 32; ++osr)
-    {
-        sbr = UART_GET_SBR(SystemCoreClock, conf->baudRate, osr);
-        baud = UART_BAUD_RATE_GEN(SystemCoreClock, sbr, osr);
-        delta = ABS(baud - conf->baudRate);
-        if (delta < minDelta)
-        {
-            minDelta = delta;
-            minOsr = osr;
-            minSbr = sbr;
-        }
-    }
-
+    sbr = SystemCoreClock / (conf->baudRate * 16);
+    /* write the sbr value to the BDH and BDL registers*/
     UART0->BDL &= ~UART0_BDL_SBR_MASK;
-    UART0->BDL |= UART0_BDL_SBR(((uint8_t *)&minSbr)[0]);
+    UART0->BDL |= UART0_BDL_SBR(((uint8_t *)&sbr)[0]);
     UART0->BDH &= ~UART0_BDH_SBR_MASK;
-    UART0->BDH |= UART0_BDH_SBR(((uint8_t *)&minSbr)[1]);
-
-    UART0->C4 &= (UART0->C4 & (~UART0_C4_OSR_MASK)) | UART0_C4_OSR(minOsr);
+    UART0->BDH |= UART0_BDH_SBR(((uint8_t *)&sbr)[1]);
 
     /* Enable the receiver and/or transmitter */
     UART0->C2 |= (conf->type & UART_TYPE_TRANSMITTER_MASK) ? UART0_C2_TE(1)
@@ -156,8 +122,27 @@ void UART_config(uint8_t uartx, uart_conf_t *conf)
     UART0->C2 |= (conf->type & UART_TYPE_RECEIVER_MASK) ? UART0_C2_RE(1)
                                                         : UART0_C2_RE(0);
     /* Enable receiver interrupt */
-    UART0->C2 |= (conf->type & UART_TYPE_RECEIVER_MASK) ? UART0_C2_RIE(1)
-                                                        : UART0_C2_RIE(0);
+    /* UART0->C2 |= (conf->type & UART_TYPE_RECEIVER_MASK) ? UART0_C2_RIE(1)
+                                                        : UART0_C2_RIE(0); */
+
+    /* Enable UART0 after setting up configuration */
+    if (conf->type & UART_TYPE_TRANSMITTER_MASK)
+    {
+        /* Enable UART0_TX*/
+        PORTA->PCR[2] |= PORT_PCR_MUX(2);
+    }
+
+    if (conf->type & UART_TYPE_RECEIVER_MASK)
+    {
+        /* Enable UART0_RX */
+        PORTA->PCR[1] |= PORT_PCR_MUX(2);
+
+        /* Enable receiver interrupt */
+        NVIC_ClearPendingIRQ(UART0_IRQn);
+        NVIC_EnableIRQ(UART0_IRQn);
+        NVIC_ClearPendingIRQ(UART0_IRQn);
+        NVIC_SetPriority(UART0_IRQn, 0);
+    }
 }
 
 void UART_sendByte(uint8_t uartx, uint8_t b)
@@ -190,6 +175,12 @@ void UART0_IRQHandler(void)
     /* Error code */
     uint32_t rc = QUEUE_ERR_NONE;
 
+    if ((UART0->S1 & UART0_S1_RDRF_MASK) == 0)
+    {
+        /* Not ready to receive => abort */
+        return;
+    };
+
     if (top == NULL)
     {
         /* Get next item if we don't have one */
@@ -206,9 +197,10 @@ void UART0_IRQHandler(void)
     /* Add received byte to the item */
     QUEUE_itemAddByte(*top, UART0->D);
 
-    if (QUEUE_itemIsFull(*top))
+    if (QUEUE_itemIsFull(*top) || (UART0->S1 & UART0_S1_IDLE_MASK))
     {
-        /* This item is full => push to queue and release it */
+        /* This item is full or receive line becomes idle
+         * => push to queue and release it */
         QUEUE_push();
         top = NULL;
     }
