23771616e03182f05d2ca4ec58f7df7e9fdf747c
Hoàng Văn Khoa <khoahv.hust@gmail.com>
2017-09-21 13:54:45 +0000

    Feature/mcp mocktest 2 improvement

--- mcp/bootloader_mocktest/loader.h
+++ mcp/bootloader_mocktest/loader.h
@@ -14,11 +14,13 @@
 /* Starting address of the application */
 #define APP_START_ADDR (0x0000A000U)
 
+#define APP_SP (*((uint32_t *)APP_START_ADDR))
+
 /* Starting address of the application's reset handler */
 #define APP_ENTRY_ADDR (0x0000A004U)
 
-/* (Maximum) Size of the application in sectors */
-#define APP_SIZE (255U)
+/* (Maximum) Size of the application in sectors (256 - 0xA000/1024) */
+#define APP_SIZE (216U)
 
 /* Entry point function pointer type */
 typedef void (*entry_point_t)(void);
@@ -33,6 +35,11 @@ typedef void (*entry_point_t)(void);
  * @return Return true if success.
  */
 bool LOADER_preload();
+
+/*!
+ * @brief Mark as no app
+ */
+void LOADER_resetState();
 
 /*!
  * @brief Write a parsed SREC line into flash memory.
